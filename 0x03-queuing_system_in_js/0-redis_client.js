4eeeb56adfd8d7ffaf1fb66c54c21f5a1af732d5
adams miracle <adamsmiracle0@gmail.com>
2024-09-28 20:25:31 +0000

    tasks done

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,13 +1,13 @@
-import { createClient } from 'redis';
+import { createClient } from "redis";
 
-function redisConnect () {
+function redisConnect() {
   const client = createClient();
 
   client
-    .on('connect', function () {
-      console.log('Redis client connected to the server');
+    .on("connect", function () {
+      console.log("Redis client connected to the server");
     })
-    .on('error', (err) => {
+    .on("error", (err) => {
       console.log(`Redis client not connected to the server: ${err}`);
     });
 }
